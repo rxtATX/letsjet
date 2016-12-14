0bc312366d9736a98dd7d168abda0fb7cb3f8858
ttruong89 <t.truong2009@gmail.com>
2016-12-13 19:04:24 -0600

    updated hbs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,22 @@
-// Scrolls down slowly to flight results section.
-$("#searchBtn").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#flightResults").offset().top},
-        'slow');
-    $("#flightResults").show();
-});
+$(document).ready(function() {
+	// // Scrolls down slowly to flight results section.
+	// $("#searchBtn").click(function() {
+	//     $('html,body').animate({
+	//         scrollTop: $("#flightResults").offset().top},
+	//         'slow');
+	//     $("#flightResults").show();
+	// });
 
-// Scrolls down slowly to book and events section.
-$(".btn-custom").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#bookInfo").offset().top},
-        'slow');
-});
+	// // Scrolls down slowly to book and events section.
+	// $(".btn-custom").click(function() {
+	//     $('html,body').animate({
+	//         scrollTop: $("#bookInfo").offset().top},
+	//         'slow');
+	// });
 
-// Show the divs for flight results once submit button is clicked.
-$("#searchBtn").click(function() {
-    $(".text-center").show();
-    $(".table-responsive").show();
+	// Show the divs for flight results once submit button is clicked.
+	$("#searchBtn").click(function() {
+		$("#flightResults").show();
+	});
 });
 
